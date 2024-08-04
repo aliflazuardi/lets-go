edd14031e30a28f88db6aa3ddd11c2d69aed3afa
aliflazuardi <aliflazuardi250999@gmail.com>
2024-08-04 14:04:18 +0700

    refactor http error method not allowed response to use http error method

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -21,8 +21,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
